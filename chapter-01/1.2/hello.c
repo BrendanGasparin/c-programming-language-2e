8c4f63151260f7c9317d1d1c480daa1f63761def
Brendan Gasparin <brendan.gasparin@gmail.com>
2025-06-20 07:12:45 +1000

    Put quotes around hello, world in the header comments

--- chapter-01/1.2/hello.c
+++ chapter-01/1.2/hello.c
@@ -2,7 +2,7 @@
     helloworld.c
     Date: Friday 20th June, 2025
     Author: Brendan Gasparin <brendan.gasparin@gmail.com>
-    Prints a hello, world message, with a \b thrown in for experimentation.
+    Prints a "hello, world" message, with a \b thrown in for experimentation.
 */
 
 #include <stdio.h>
