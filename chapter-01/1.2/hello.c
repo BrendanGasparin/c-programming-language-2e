6be87f3b07568ee5d6bf70cc8c30c3b448545d23
Brendan Gasparin <brendan.gasparin@gmail.com>
2025-06-20 07:08:57 +1000

    Completed exercise 1.2

---
+++ chapter-01/1.2/hello.c
@@ -0,0 +1,13 @@
+/*
+    helloworld.c
+    Date: Friday 20th June, 2025
+    Author: Brendan Gasparin <brendan.gasparin@gmail.com>
+    Prints a hello, world message, with a \b thrown in for experimentation.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello, world\d\n");
+}
