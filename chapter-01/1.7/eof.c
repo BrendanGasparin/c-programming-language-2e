60d048ccbc56d1adbc74b68d3f5accc5f88c6399
Brendan Gasparin <brendan.gasparin@gmail.com>
2025-06-21 18:14:11 +1000

    Implement exercise 1.7

---
+++ chapter-01/1.7/eof.c
@@ -0,0 +1,13 @@
+/*
+    eof.c
+    Date: Saturday 21st June 2025
+    Author: Brendan Gasparin <brendan.gasparin@gmail.com>
+    Prints the value of EOF
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("%i\n", EOF);
+}
