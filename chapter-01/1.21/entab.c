8775fd747ce38fae09eef4217e05bc064895b229
Brendan Gasparin <brendan.gasparin@gmail.com>
2025-07-03 07:40:27 +1000

    Write non-working skeleton of entab.c

---
+++ chapter-01/1.21/entab.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define COLUMN_SIZE 4
+#define CHAR_BUFFER_SIZE 2500
+
+int get_line(char s[], int lim);
+
+int main()
+{
+    char line[CHAR_BUFFER_SIZE];    // The line buffer
+    int length;                     // Length of the line
+
+    while (length = get_line(line, CHAR_BUFFER_SIZE) > 0)
+        entab(line, length);
+
+    return 0;
+}
+
+// getline: read a line into s, return length
+int get_line(char s[], int lim)
+{
+    int c, i;
+    int limit;
+
+    for (i = 0, limit = lim - 1; i < limit && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+
+    return i;
+}
+
+void entab(char s[], int length) {
+    return;
+}
