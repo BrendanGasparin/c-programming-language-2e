7129187011baf1994704e3e83e38b88034ce9009
Brendan Gasparin <brendan.gasparin@gmail.com>
2025-07-03 07:29:20 +1000

    Implement exercise 1.20

---
+++ chapter-01/1.20/detab.c
@@ -0,0 +1,77 @@
+/*
+    detab.c
+    Date: Sunday 29th June 2025
+    Author: Brendan Gasparin <brendan.gasparin@gmail.com>
+    Converts tabs from the input into an appropriate number of spaces
+    in the output.
+*/
+
+#include <stdio.h>
+
+#define CHAR_BUFFER_SIZE 2500
+#define COLUMNS 625
+#define TRUE 1
+#define FALSE 0
+
+void detab(char s[], int length);
+int get_line(char s[], int lim);
+
+int main()
+{
+    char line[CHAR_BUFFER_SIZE];    // The line buffer
+    int length;                     // Length of the line
+
+    while (length = get_line(line, CHAR_BUFFER_SIZE) > 0)
+        detab(line, length);
+
+    return 0;
+}
+
+// getline: read a line into s, return length
+int get_line(char s[], int lim)
+{
+    int c, i;
+    int limit;
+
+    for (i = 0, limit = lim - 1; i < limit && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+
+    return i;
+}
+
+// Prints the input, converting tabs into the sppropriate number of space characters
+void detab(char s[], int length)
+{
+    int char_count = 0;
+    int n;  // Number of spaces to print
+    int i, j;
+
+    for (i = 0; s[i] != '\0'; i++) {
+        if (s[i] == '\t') {
+            if (char_count % 4 == 0)
+                n = 4;
+            else if (char_count % 4 == 1)
+                n = 3;
+            else if (char_count % 4 == 2)
+                n = 2;
+            else
+                n = 1;
+
+            for (j = 0; j < n; j++)
+            {
+                putchar(' ');
+                char_count++;
+            }
+        }
+        else
+        {
+            putchar(s[i]);
+            char_count++;
+        }
+    }
+}
